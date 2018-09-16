8a870c0c5ff27c1e4dd90e62ddde53fd6fe010ff
Yoshinobu Ota <yoshinobuota@KaoriOtaAir.local>
2018-09-16 10:35:20 +0900

    edit data file name

--- data/manage_poubelle.sh
+++ data/manage_poubelle.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #add schema
-curl -X POST -H 'Content-type:text/xml' --data-binary @poubelle_data.xml http://localhost:8983/solr/trash/update?commit=true
+curl -X POST -H 'Content-type:text/xml' --data-binary @poubelle_data_A.xml http://localhost:8983/solr/trash/update?commit=true
 #curl -X POST -d @myfilename http://user:pass@myhost/hudson/job/_jobName_/postBuildResult
 
