453ce4dad915870c7f6cf98b3327d48d33a9d699
Yoshinobu Ota <yoshinobuota@KaoriOtaAir.local>
2018-09-30 17:58:44 +0900

    debug

--- app.py
+++ app.py
@@ -117,7 +117,8 @@ def handle_message(event):
         )
 
         # get request to solr
-        response = requests.get('http://4d8d496e.ngrok.io/solr/trash/select', params=params)
+        #response = requests.get('http://4d8d496e.ngrok.io/solr/trash/select', params=params)
+        response = requests.get('http://47db6ec5.ngrok.io/solr/trash/select', params=params)
         # Original curl command
         # curl http://localhost:8983/solr/trash/select?indent=on&q=trash_name:雨戸&wt=json
         # response = requests.get('http://localhost:8983/solr/trash/select?indent=on&q=trash_name:雨戸&wt=json')
