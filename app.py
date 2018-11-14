b2feef499630cff599803311feabc57855512f6b
28kayak <kaya.ota@gmail.com>
2018-11-14 09:31:57 +0900

    + app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#
+#
+#
 import os
 import sys
 import requests
@@ -30,6 +33,16 @@ if channel_access_token is None:
 
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
+
+trash_schedule = {
+    0: "燃えないゴミ　・　有害危険ゴミ",
+    1: "燃えるゴミ",
+    2: "資源物１類　 ビン・カン・ペットボトル",
+    3: "捨てられるゴミはありません",
+    4: "燃えるゴミ",
+    5: "捨てられるゴミはありません",
+    6: "資源回収（資源２類）古着・新聞紙・雑誌"
+}
 
 
 @app.route("/")
@@ -71,7 +84,7 @@ def push_trash():
     # 5 : Sat
     # 6 : Sun
 
-    trash_schedule = {
+    """trash_schedule = {
         0: "燃えないゴミ　・　有害危険ゴミ",
         1: "燃えるゴミ",
         2: "資源物１類　 ビン・カン・ペットボトル",
@@ -79,7 +92,7 @@ def push_trash():
         4: "燃えるゴミ",
         5: "捨てられるゴミはありません",
         6: "資源回収（資源２類）古着・新聞紙・雑誌"
-    }
+    }"""
     weekday = datetime.now().weekday()
     return_text += trash_schedule[weekday]
     to = "Ua21ed0b7a6399fad17ba2adfd9efaa03"
@@ -92,15 +105,7 @@ def push_trash():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = " "
-    trash_schedule = {
-        0: "燃えないゴミ　・　有害危険ゴミ",
-        1: "燃えるゴミ",
-        2: "資源物１類　 ビン・カン・ペットボトル",
-        3: "捨てられるゴミはありません",
-        4: "燃えるゴミ",
-        5: "捨てられるゴミはありません",
-        6: "資源回収（資源２類）古着・新聞紙・雑誌"
-    }
+
     weekday = datetime.now().weekday()
     if event.message.text == "今日":
         text = trash_schedule[weekday]
@@ -119,7 +124,7 @@ def handle_message(event):
         )
 
         # get request to solr
-        #response = requests.get('http://4d8d496e.ngrok.io/solr/trash/select', params=params)
+        # response = requests.get('http://4d8d496e.ngrok.io/solr/trash/select', params=params)
         response = requests.get('http://47db6ec5.ngrok.io/solr/trash/select', params=params)
 
         # Original curl command
@@ -148,11 +153,11 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text))
-        # TextSendMessage(text=event.message.text))
+
 
 
 if __name__ == "__main__":
-    #app.run()
+
     port = int(os.getenv('PORT', 5000))
     app.run(debug=True, host='0.0.0.0',
             port=port)
